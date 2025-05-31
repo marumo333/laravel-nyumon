ca301a40734f6ef5cf1a7d21ba13b1df93d682a6
marumo333 <seito5026junior@icloud.com>
2025-06-01 01:54:14 +0900

    station6クリア

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiaryController;
 
-Route::get('/diary', function () {
-    return 'Hello!';
-});
+Route::get('/diary', [DiaryController::class, 'index'])->name('diary.index');
