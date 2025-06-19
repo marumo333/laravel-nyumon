7225deee7eff08e62ac76de8fa347cf4f71620ef
marumo333 <seito5026junior@icloud.com>
2025-06-19 19:26:21 +0900

    station9クリア

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Diary;     
 
 class DiaryController extends Controller
 {
     //
     public function index() {
+    $diaries = Diary::all();
     $name = 'Laravel';
-    return view('diary.index', compact('name'));
+    return view('diary.index', compact('name','diaries'));
 }
 }
--- resources/views/diary/index.blade.php
+++ resources/views/diary/index.blade.php
@@ -3,4 +3,9 @@
 Hello, {{ $name }}
 </body>
 </html>
-
+@foreach($diaries as $diary)
+<div>
+  <div>{{ $diary->date }}</div>
+  <div>{{ $diary->title }}</div>
+</div>
+@endforeach
